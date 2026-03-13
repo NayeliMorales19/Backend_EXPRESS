09542827893d94c1bd26c2b88377515c2e7dc449
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-13 00:28:13 -0600

    correcciones funcion crear

--- controller/alumnoController.js
+++ controller/alumnoController.js
@@ -8,24 +8,24 @@ exports.create = function(req,res){
 
 const alumno = new Alumno({
 
-NumControl: req.body.num_control,
-Nombre: req.body.nombre,
-PrimerAp: req.body.primer_ap,
-SegundoAp: req.body.segundo_ap,
-FechaNac: req.body.fecha_nac,
-Semestre: req.body.semestre,
-Carrera: req.body.carrera
+NumControl:req.body.num_control,
+Nombre:req.body.nombre,
+PrimerAp:req.body.primer_ap,
+SegundoAp:req.body.segundo_ap,
+FechaNac:req.body.fecha_nac,
+Semestre:req.body.semestre,
+Carrera:req.body.carrera
 
 });
 
-Alumno.create(alumno,function(err){
+Alumno.create(alumno,function(err,data){
 
 if(err){
 console.log(err);
 return res.status(500).send(err);
 }
 
-res.redirect('/');
+res.send("Alumno guardado correctamente");
 
 });
 
