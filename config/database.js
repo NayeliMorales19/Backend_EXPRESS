383b04bc02b220ccbc3a7ffeb669fa1e045e1d63
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-13 01:32:08 -0600

    funcion

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "mysql-5eec0ff-moralesnayeli120217-9a72.g.aivencloud.com",
-  user: process.env.DB_USER || "avnadmin",
-  password: process.env.DB_PASSWORD || "AVNS_6zWIvQyWkIcXPnPSttK",
-  database: process.env.DB_NAME || "defaultdb",
-  port: process.env.DB_PORT || 10600,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 
   waitForConnections: true,
   connectionLimit: 10,
